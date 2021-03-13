4e42c243bff2a8c99b195c1509ca90a9e852f811
Lathas0123 <lathas2020ls@gmail.com>
2021-03-12 19:52:33 -0800

    new file

---
+++ pyton_day4_batch38.py
@@ -0,0 +1,23 @@
+fname="kiran"
+print(fname)
+
+
+fullname="girija swamy "
+print(fullname.title())
+print(fullname.upper())
+print(fullname.lower())
+
+
+pname="python"
+pname2="is  easy programming language"
+
+par=f"{pname} {pname2}"
+print(par.title())
+
+print('fav_lang:pythoncc++javahtmlcssjavascript')
+
+print('fav_lang:python\nc\nc++\njava\nhtml\ncss\njavascript')
+print('fav_lang:python\n\tc\n\tc++\n\tjava\n\thtml\n\tcss\n\tjavascript')
+
+
+
