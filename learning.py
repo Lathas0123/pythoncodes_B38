fa6debd6fee3423ad0a15d61d6feca3bea8d64a3
Lathas0123 <lathas2020ls@gmail.com>
2021-04-16 19:24:01 -0700

    added files

--- learning.py
+++ learning.py
@@ -13,4 +13,37 @@ print(floor(4.4))
 print(sqrt(144))
 
 l=input("enter your name   ")
-print("welcome "+l)
+
+a=input("enter your age  ")
+print("welcome "+l+"   your age is"+a)
+
+#simple calculator
+num1=input("enter a first number")
+num2=input("enter 2nd number")
+result=int(num1)+int(num2)
+print(result)
+
+num1=input("enter a first number")
+num2=input("enter 2nd number")
+result=int(num1)-int(num2)
+print(result)
+
+num1=input("enter a first number")
+num2=input("enter 2nd number")
+result=float(num1)*float(num2)
+print(result)
+
+num1=input("enter a first number")
+num2=input("enter 2nd number")
+result=float(num1)/float(num2)
+print(result)
+
+#mad libs Game
+
+name=input("Enter your name")
+pronoun=input("Enter something")
+celebrity=input("enter celebrity name")
+
+print("Your name is  "+name)
+print(pronoun+"you are hobbie")
+print("my favorite actor"+celebrity)
