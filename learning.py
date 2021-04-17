e63c273a40a895232c6424e6a6dc947986f79023
Lathas0123 <lathas2020ls@gmail.com>
2021-04-16 19:03:52 -0700

    added files

---
+++ learning.py
@@ -0,0 +1,16 @@
+from math import*
+print (3+5*8)
+
+my_num=5
+print("my favorite number is"+ str(my_num))
+x=-5
+print(pow(3,2))
+print(max(4,6))
+print(min(8,9))
+print(round(8.9))
+print(ceil(4.2))
+print(floor(4.4))
+print(sqrt(144))
+
+l=input("enter your name   ")
+print("welcome "+l)
