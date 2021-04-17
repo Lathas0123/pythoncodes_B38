ba723031446be102591a9ea4fca31fb5c60ab90e
Lathas0123 <lathas2020ls@gmail.com>
2021-04-16 20:11:34 -0700

    added files

--- learning.py
+++ learning.py
@@ -1,6 +1,6 @@
 from math import*
 print (3+5*8)
-
+"""
 my_num=5
 print("my favorite number is"+ str(my_num))
 x=-5
@@ -16,8 +16,9 @@ l=input("enter your name   ")
 
 a=input("enter your age  ")
 print("welcome "+l+"   your age is"+a)
-
+"""
 #simple calculator
+"""
 num1=input("enter a first number")
 num2=input("enter 2nd number")
 result=int(num1)+int(num2)
@@ -37,9 +38,9 @@ num1=input("enter a first number")
 num2=input("enter 2nd number")
 result=float(num1)/float(num2)
 print(result)
-
+"""
 #mad libs Game
-
+"""
 name=input("Enter your name")
 pronoun=input("Enter something")
 celebrity=input("enter celebrity name")
@@ -47,3 +48,24 @@ celebrity=input("enter celebrity name")
 print("Your name is  "+name)
 print(pronoun+"you are hobbie")
 print("my favorite actor"+celebrity)
+"""
+#tuple
+"""
+fri=["latha","suma","rishi","kulli","gowri"]
+fri[0]="kiran"
+print(fri)
+"""
+#list
+
+num=[4,8,12,16,20,24]
+fri=["latha","suma","rishi","kulli","gowri"]
+fri.extend(num)
+fri.append("kiran")
+fri.insert(1,"kelvin")
+fri.remove("kelvin")
+num.sort()
+num.reverse()
+print(num)
+print(fri)
+
+
