a090656275898d3bf22c4dc43f2a7d889c4cb008
Lathas0123 <lathas2020ls@gmail.com>
2021-04-17 18:53:29 -0700

    few of codes added

---
+++ functions.py
@@ -0,0 +1,59 @@
+"""
+fname="kiran"
+print(fname)
+
+#using title uppper and lower methods.
+fullname="girija swamy "
+print(fullname.title())
+print(fullname.upper())
+print(fullname.lower())
+
+# concatinating 2 variables.
+pname="python"
+pname2="is  easy programming language"
+
+par=f"{pname} {pname2}"
+print(par.title())
+# printing values line by line.
+print('fav_lang:pythoncc++javahtmlcssjavascript')
+
+print('fav_lang:python\nc\nc++\njava\nhtml\ncss\njavascript')
+print('fav_lang:python\n\tc\n\tc++\n\tjava\n\thtml\n\tcss\n\tjavascript')
+"""
+tuple=[(3,4) ,(4,3) ,(8,9)]
+print(tuple)
+
+def sayhi():
+    print("welcome to function chapter")
+sayhi()
+"""
+def xvalue(name,usn,college):
+    x=input("Enter your name")
+    y=input("enter your usn")
+    z=input("Enter your college")
+     print("Hello"+strx)
+     print("your usn no is"+y)
+     print("you are from"+z)
+xvalue("latha","16","gce")
+"""
+# functions with parameters
+"""
+def student(name):
+
+    name1=input("enter your name")
+    print("hi"+name1)
+    print("hello"+name)
+
+student(name="suma")
+"""
+#using return keyword
+
+def cube(num):
+    return num*num*num
+print(cube(5))
+
+def squr(num2):
+    return num2*num2
+
+print(squr(4))
+
